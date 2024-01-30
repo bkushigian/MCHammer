ab1f41605232116e781241e2a4abe8c31b5863e2
bkushigian <bkushigian@gmail.com>
2024-01-29 16:35:53 -0800

    Updates

--- msav.sh
+++ msav.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd -P "$( dirname "$(realpath "${BASH_SOURCE[0]}")" )" && pwd )"
 POM_XML="${SCRIPT_DIR}/pom.xml"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
-mvn -f ${POM_XML} exec:java -Dexec.mainClass="org.mutation_testing.App" -Dexec.args="$*"
+mvn -f "${POM_XML}" exec:java -Dexec.mainClass="org.mutation_testing.App" -Dexec.args="$*"
