f3789e7362c87d6f36e13695eb4aab2ccbfc5a39
bkushigian <bkushigian@gmail.com>
2024-01-30 21:30:32 -0800

    Fat jar

--- msav.sh
+++ msav.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd -P "$( dirname "$(realpath "${BASH_SOURCE[0]}")" )" && pwd )"
-POM_XML="${SCRIPT_DIR}/pom.xml"
+UBER_JAR="${SCRIPT_DIR}/target/abstract-state-mutator-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
-mvn -f "${POM_XML}" exec:java -Dexec.mainClass="org.mutation_testing.App" -Dexec.args="$*"
+# POM_XML="${SCRIPT_DIR}/pom.xml"
+# mvn -f "${POM_XML}" exec:java -Dexec.mainClass="org.mutation_testing.App" -Dexec.args="$*"
+
+echo "$*"
+java -cp "${UBER_JAR}" org.mutation_testing.App "$@"
