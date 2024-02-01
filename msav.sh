06d4df16f32befeb36adf79ccf6d1bb409cb0f01
bkushigian <bkushigian@gmail.com>
2024-02-01 12:31:08 -0800

    Removed print in script

--- msav.sh
+++ msav.sh
@@ -6,5 +6,4 @@ UBER_JAR="${SCRIPT_DIR}/target/abstract-state-mutator-1.0-SNAPSHOT-jar-with-depe
 # POM_XML="${SCRIPT_DIR}/pom.xml"
 # mvn -f "${POM_XML}" exec:java -Dexec.mainClass="org.mutation_testing.App" -Dexec.args="$*"
 
-echo "$*"
 java -cp "${UBER_JAR}" org.mutation_testing.App "$@"
