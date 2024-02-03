58788e87eb5ae8bc3616be9b12c3104fa31a3386
bkushigian <bkushigian@gmail.com>
2024-02-02 20:32:35 -0800

    Fixed msav.sh to build fat jar if needed

--- msav.sh
+++ msav.sh
@@ -6,4 +6,9 @@ UBER_JAR="${SCRIPT_DIR}/target/abstract-state-mutator-1.0-SNAPSHOT-jar-with-depe
 # POM_XML="${SCRIPT_DIR}/pom.xml"
 # mvn -f "${POM_XML}" exec:java -Dexec.mainClass="org.mutation_testing.App" -Dexec.args="$*"
 
+if [ ! -f "${UBER_JAR}" ]; then
+    echo "Building the project..."
+    mvn -f "${SCRIPT_DIR}/pom.xml" clean package shade:shade
+fi
+
 java -cp "${UBER_JAR}" org.mutation_testing.App "$@"
