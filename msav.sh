2a004d0c565cac494a87b80b324d0d0f1028e078
bkushigian <bkushigian@gmail.com>
2024-03-17 06:58:26 -0600

    Updated name to mchammer

--- msav.sh
+++ msav.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd -P "$( dirname "$(realpath "${BASH_SOURCE[0]}")" )" && pwd )"
-UBER_JAR="${SCRIPT_DIR}/target/abstract-state-mutator-1.0-SNAPSHOT-jar-with-dependencies.jar"
+UBER_JAR="${SCRIPT_DIR}/target/MCHammer-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 # POM_XML="${SCRIPT_DIR}/pom.xml"
 # mvn -f "${POM_XML}" exec:java -Dexec.mainClass="org.mutation_testing.App" -Dexec.args="$*"
